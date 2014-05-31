a90b3661b2b302bfe16e4bd3e766463e01fdb072
gborobio73 <gborobio@gmail.com>
2014-05-31 12:28:38 +0300

    comments

---
+++ virtual-is-viral-war/src/main/java/com/leeloo/viv/rest/Comment.java
@@ -0,0 +1,15 @@
+package com.leeloo.viv.rest;
+
+public class Comment{
+  
+  public String Id;
+  public String User;
+  public String Comment; 
+ 
+  public Comment(String id, String user, String comment)
+  {
+    Id = id;
+    User = user;
+    Comment = comment;
+  }
+}
