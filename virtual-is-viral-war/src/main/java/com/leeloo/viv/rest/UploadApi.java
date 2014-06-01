bf80297332b80e271c6baca6c6ab1dea9c9f3b27
gborobio73 <gborobio@gmail.com>
2014-06-01 15:37:47 +0300

    upload

---
+++ virtual-is-viral-war/src/main/java/com/leeloo/viv/rest/UploadApi.java
@@ -0,0 +1,40 @@
+package com.leeloo.viv.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.NewCookie;
+import java.util.List;
+
+import com.google.gson.Gson;
+
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import com.google.appengine.api.users.User;
+
+import com.google.appengine.api.blobstore.BlobKey;
+import com.google.appengine.api.blobstore.BlobstoreService;
+import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
+
+
+@Path("upload")
+public class UploadApi {
+    @GET
+    @Path("url")
+    @Produces({ MediaType.APPLICATION_JSON })
+    public Response getUrl() {
+
+        String uploadFileURL="";
+        UserService userService = UserServiceFactory.getUserService();
+        
+        BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
+        
+        uploadFileURL=blobstoreService.createUploadUrl("/testurl");
+
+        return Response.ok().entity(new Gson().toJson(uploadFileURL)).build();
+
+    }
+}
